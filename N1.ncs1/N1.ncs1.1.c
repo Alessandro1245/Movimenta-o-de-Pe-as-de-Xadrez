100b2a39f8a5d0982711c528329823b078f21ae4
Alessandro1245 <ab8984698@gmail.com>
2025-04-15 08:47:17 +0000

    Movimentação de Peças de Xadrez

---
+++ N1.ncs1/N1.ncs1.1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(){
+for (int i = 1; i <= 5; i++ ) {
+     printf("%d\n", i);
+     printf("Bispo a Cima, Direita\n");
+}
+
+int a = 1;
+
+while (a <= 7)
+{
+    printf("%d\n", a);
+    printf("Torre a direita\n");
+    a++;
+}
+
+int e = 1;
+
+do
+{
+    printf("%d\n", e);
+    printf("Rainha a esquerda\n");
+    e++;
+} while (e <= 8);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
